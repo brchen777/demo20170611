17160bd180ec242f720f010b399f8f7de01128c8
brchen777 <eric790620@gmail.com>
2017-06-11 10:11:02 +0800

    練習 routerLink 路由連結的語法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
     enableTracing: true,
-    useHash: true
+    useHash: false
   })],
   exports: [RouterModule]
 })
