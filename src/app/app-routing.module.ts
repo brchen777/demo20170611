3da07727f2109e883b81a5256328b18c7498352f
brchen777 <eric790620@gmail.com>
2017-06-11 13:40:36 +0800

    練習 Angular 子路由設定方式並建立 /charts/flot 頁面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { fullbackRoute } from './shared/fullback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { FlotComponent } from './charts/flot/flot.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'cards', component: CardsComponent },
+  { path: 'charts', children: [
+    { path: 'flot', component: FlotComponent }
+  ] },
   fullbackRoute
 ];
 
