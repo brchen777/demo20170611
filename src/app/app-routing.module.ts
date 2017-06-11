23dc61381e6b16c8c588503487737bcd6496f091
brchen777 <eric790620@gmail.com>
2017-06-11 16:43:13 +0800

    加入 login 與 layout 元件與設定路由，並讓 layout 元件的路由變成所有現有元件的上層路由

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,19 @@ import { Routes, RouterModule } from '@angular/router';
 import { fullbackRoute } from './shared/fullback-route';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { CardsComponent } from './cards/cards.component';
+import { LoginComponent } from './login/login.component';
+import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'cards/:type', component: CardsComponent },
-  { path: 'charts', loadChildren: './charts/charts.module.ts#ChartsModule' },
+  { path: '', component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'dashboard', component: DashboardComponent },
+      { path: 'cards/:type', component: CardsComponent },
+      { path: 'charts', loadChildren: './charts/charts.module.ts#ChartsModule' }
+    ]
+  },
+  { path: 'login', component: LoginComponent },
   fullbackRoute
 ];
 
