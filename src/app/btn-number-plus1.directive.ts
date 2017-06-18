aa390ab24aa6ebd76fe2a8a6279d598795af5222
brchen777 <eric790620@gmail.com>
2017-06-18 17:38:53 +0800

    示範 Attribute Directive 如何傳入參數

--- src/app/btn-number-plus1.directive.ts
+++ src/app/btn-number-plus1.directive.ts
@@ -1,16 +1,18 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[btn-plus1]'
 })
 export class BtnNumberPlus1Directive {
 
+  @Input() myTitle: string = '';
+
   constructor() { }
 
   @HostListener('click', ['$event.target'])
   plus1(btn: HTMLButtonElement) {
-    let num = +btn.innerText;
+    let num = parseInt(btn.innerText, 10);
     num++;
-    btn.innerText = num.toString();
+    btn.innerText = num.toString() + this.myTitle;
   }
 }
